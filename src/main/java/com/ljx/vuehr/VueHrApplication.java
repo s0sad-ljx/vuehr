5e61140b8143981d6819961bdceccc0744fdeda0
ljx <252142810@qq.com>
2020-09-24 14:10:34 +0800

    first commit

---
+++ src/main/java/com/ljx/vuehr/VueHrApplication.java
@@ -0,0 +1,13 @@
+package com.ljx.vuehr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueHrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueHrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ljx/vuehr/VueHrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljx.vuehr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueHrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
